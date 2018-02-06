f2bb7fd11b7cabd982b2d5f65b668c6f56839721
Roberto Ribeiro <robertov82008@gmail.com>
2018-02-06 13:34:59 -0200

    26.9 Recebendo mensagem com o aplicativo aberto 26.12 Mensagens do tipo Dados (Data Messages)

--- app/src/main/java/br/com/livroandroid/hellofirebase/MainActivity.kt
+++ app/src/main/java/br/com/livroandroid/hellofirebase/MainActivity.kt
@@ -2,11 +2,18 @@ package br.com.livroandroid.hellofirebase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "firebase"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Ao clicar na notificação os parâmetros são enviados pela intent
+        Log.d(TAG, "Nome: ${intent.getStringExtra("nome")}")
+        Log.d(TAG, "Sobrenome: ${intent.getStringExtra("sobrenome")}")
     }
 }
--- app/src/main/java/br/com/livroandroid/hellofirebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/br/com/livroandroid/hellofirebase/MyFirebaseMessagingService.kt
@@ -1,6 +1,8 @@
 package br.com.livroandroid.hellofirebase
 
+import android.content.Intent
 import android.util.Log
+import br.com.livroandroid.hellofirebase.extensions.toBundle
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -10,7 +12,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "onMessageReceived")
-
+        // Verifica se a mensagem é do tipo dados (data messages)
+        if (remoteMessage?.data.isNotEmpty()) {
+            val data = remoteMessage.data
+            Log.d(TAG, "Dados: $data")
+            Log.d(TAG, "Nome: ${data["nome"]}")
+            Log.d(TAG, "Sobrenome: ${data["sobrenome"]}")
+        }
         // Verifica se a mensagem é do tipo notificação.
         if (remoteMessage?.notification != null) {
             val title = remoteMessage.notification?.title
@@ -18,6 +26,22 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
             Log.d(TAG, "Message Notification Title: $title")
             Log.d(TAG, "Message Notification Body: $msg")
+
+            showNotification(remoteMessage)
         }
+    }
+
+    // Mostra uma notificação
+    private fun showNotification(remoteMessage: RemoteMessage) {
+        // Intent para abrir a MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        // Adiciona os parâmetros na intent
+        intent.putExtras(remoteMessage.data.toBundle())
+        // Title: O título é opcional...
+        val title = remoteMessage.notification?.title ?: getString(R.string.app_name)
+        // Mensagem
+        val msg = remoteMessage.notification?.body!!
+        // Cria uma notificação.
+        NotificationUtil.create(this, 1, intent, title, msg)
     }
 }
